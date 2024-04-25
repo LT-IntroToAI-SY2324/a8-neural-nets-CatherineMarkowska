6057cf5d52c0c68ee48a1563fb4cbd11a2673887
Catherine Markowska <cimarkowska@cps.edu>
2024-04-25 10:09:48 -0500

    worked on uci

--- neural_net_UCI_data.py
+++ neural_net_UCI_data.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 from neural import *
+from sklearn.model_selection import train_test_split
 
 
 def parse_line(line: str) -> Tuple[List[float], List[float]]:
@@ -51,9 +52,15 @@ with open("wine_data.txt", "r") as f:
 # print(training_data)
 
 td = normalize(training_data)
-print(td)
-# nn = NeuralNet(13, 3, 1)
-# nn.train(td, iters=100_000, print_interval=1000, learning_rate=0.1)
+print(len(td))
 
-# for i in nn.test_with_expected(td):
-#     print(f"desired: {i[1]}, actual: {i[2]}")
+train, test = train_test_split(td)
+print(len(train))
+print(len(test))
+print(test)
+
+nn = NeuralNet(13, 3, 1)
+nn.train(train, iters=10000, print_interval=1000, learning_rate=0.1)
+
+for i in nn.test_with_expected(td):
+    print(f"desired: {i[1]}, actual: {i[2]}")
